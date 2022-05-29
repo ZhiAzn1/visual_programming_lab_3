4dba1c050c52735123b42cad820ceacefd20a9a6
ZhiAzn <zhiazn@mail.ru>
2022-05-29 19:35:14 +0700

    lab_3

---
+++ Lab/Program.cs
@@ -0,0 +1,15 @@
+// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using HomeWork1;
+
+
+class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
